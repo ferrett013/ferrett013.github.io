3a2785bddc7ba92cb22d1209a1a5f2a0c827e79a
Garrett Fardon <garrett.fardon@gmail.com>
2015-04-26 13:44:00 -0600

    Basic text

--- index.php
+++ index.php
@@ -1,7 +1,50 @@
 <!DOCTYPE html>
 <html>
 <body>
-<h1>Hello World</h1>
-<p>I'm hosted with GitHub Pages.</p>
+<h1>Garrett Fardon</h1>
+
+<h2>Technical Skills</h2>
+<ul>
+<li>Microsoft Suite (Access, Excel and Word)</li>
+<li>Google Apps Suite (including Fusion Tables and Google Apps Script)</li>
+<li>Votebuilder</li>
+<li>Wordpress</li>
+<li>Photoshop</li>
+<li>InDesign</li>
+<li>PHP</li>
+<li>SQL (MySQL, PostgreSQL, Redshift and PostGIS variants)</li>
+<li>HTML</li>
+<li>CSS</li>
+<li>Quantum GIS</li>
+<li>Leaflet.js</li>
+</ul>
+
+Experience
+Colorado Democratic Party, Data Deputy
+• Reported on over 5 million contact attempts from a backend database in legible and concise reports.
+• Rapidly developed reports for analytics purposes.
+• Helped shape the development of various voter models, including support, turnout, persuasion and
+volunteer likelihood.
+• Created flexible mapping tools for geographic resource allotment.
+• Provided technical assistance on our internal data tools, such as our mapping systems and voterfile.
+Iowa Democratic Party, Data and Technology Director February 2013-May 2014
+• Led a data-driven state party program by improving technology literacy and information access for activists across the state through trainings both in-person and online.
+• Managed and mediated statewide Votebuilder (a voterfile management system) access for thousands of users and hundreds of separate committees.
+• Updated and created multiple websites.
+• Worked with the creation and use of voter models.
+• Created goals and expected turnout models for multiple elections.
+• Developed custom-made web tools for fundraising events and the Iowa caucuses that were used by
+hundred of simultaneous users.
+Organizing for America (2012 Obama Reelection), Iowa Deputy Data Director May-November 2012
+• Where I developed the skills to use and taught others the use of Excel, Access, SQL, PHP and HTML.
+• Provided trainings on Votebuilder and fulfilled a support role for the program.
+• Created reporting workflows that summarized over 6 million voter contact data points over the course
+of the election.
+• Developed statewide Election Day reporting and volunteer activation tools that were used by over a
+thousand employees and statewide activists.
+Iowa Democratic Party, IT Assistant May-November 2010
+• Participated in multiple fundraisers, speeches and high-level meetings providing technical support.
+• Provided general troubleshooting advice.
+• Consolidated soft reporting from our field team into manageable reports.
 </body>
 </html>
